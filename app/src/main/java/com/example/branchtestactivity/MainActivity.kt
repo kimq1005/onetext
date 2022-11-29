94687355895544acd654806a7717769d2bb7dad9
ringringimac <tpwnd103502@naver.com>
2022-11-29 15:29:28 +0900

    mearge test

--- app/src/main/java/com/example/branchtestactivity/MainActivity.kt
+++ app/src/main/java/com/example/branchtestactivity/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //i am branch1
-        //i am branch1
+        //branch2
     }
 }
