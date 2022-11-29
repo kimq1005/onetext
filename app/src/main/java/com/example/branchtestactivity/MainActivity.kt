48abd72667aabfc09edd67521b2fdca351a798af
ringringimac <tpwnd103502@naver.com>
2022-11-29 16:01:47 +0900

    mearge test

--- app/src/main/java/com/example/branchtestactivity/MainActivity.kt
+++ app/src/main/java/com/example/branchtestactivity/MainActivity.kt
@@ -7,9 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //여긴 먹킷을 한상태야 먹킷 ㅇㅋㅇㅋ?
-        //나는 이제 faq qa를 완료한 상태야 이 부분을 먹킷으로 보낼거야!
-
-        //위에는 faq가 적용된 먹킷이야 이부분을 faq qa 브런치로 보낼거야!
+        //여긴 먹킷리스트
     }
 }
