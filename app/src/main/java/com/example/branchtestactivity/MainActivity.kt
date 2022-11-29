078844a3d369b46190378c81014064259a1f3946
ringringimac <tpwnd103502@naver.com>
2022-11-29 15:54:56 +0900

    mearge test

--- app/src/main/java/com/example/branchtestactivity/MainActivity.kt
+++ app/src/main/java/com/example/branchtestactivity/MainActivity.kt
@@ -7,7 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //나는 브런치2야 1이 오려면 이 주석 밑으로 와야해
-        //나도 브런치2로 갈거야 근데 브런치1에는 있어야해!
+        //여긴 먹킷을 한상태야 먹킷 ㅇㅋㅇㅋ?
     }
 }
