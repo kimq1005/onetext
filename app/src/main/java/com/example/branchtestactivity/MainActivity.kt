7430442406726048e670fc27b134451c6412c34c
ringringimac <tpwnd103502@naver.com>
2022-11-29 15:33:11 +0900

    mearge test

--- app/src/main/java/com/example/branchtestactivity/MainActivity.kt
+++ app/src/main/java/com/example/branchtestactivity/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //i am branch1
+        //i am branch king
     }
 }
