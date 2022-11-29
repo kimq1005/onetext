d77507b03c08cf8b49b5d63de34c7dcb13760e1d
ringringimac <tpwnd103502@naver.com>
2022-11-29 16:08:06 +0900

    mearge test

--- app/src/main/java/com/example/branchtestactivity/MainActivity.kt
+++ app/src/main/java/com/example/branchtestactivity/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //FAQ QA 완료
+        //먹킷 진짜 완료
     }
 }
