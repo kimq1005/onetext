2ff76724e8e13071ae636b42b5b6005c84d3ae54
ringringimac <tpwnd103502@naver.com>
2022-11-29 15:15:13 +0900

    mearge test

--- app/src/main/java/com/example/branchtestactivity/MainActivity.kt
+++ app/src/main/java/com/example/branchtestactivity/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //i am branch1
+        //i am branch1
     }
 }
